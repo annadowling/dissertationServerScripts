1c00fa1d9f532001f6c8b449e14c7832e9585745
annadowling <anna.dowling@errigal.com>
2020-03-09 13:47:37 +0000

    added cpu script

--- cpu_usage.sh
+++ cpu_usage.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-
-CPU_USAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
-
-DATE=$(date "+%Y-%m-%d %H:%M:")
-
-CPU_USAGE="$DATE CPU: $CPU_USAGE"
-
-echo $CPU_USAGE >> /opt/cpu_usage.out
+#!/bin/sh
+PROCESS="java"
+CPU=`top | grep $PROCESS`
+echo $CPU >> cpu_usage.out
