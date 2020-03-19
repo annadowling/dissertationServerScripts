c806c89f84da218970dae0f463d6f442adc40104
annadowling <anna.dowling@errigal.com>
2020-03-19 15:45:49 +0000

    Added datamining results

--- exportTestResults.sql
+++ exportTestResults.sql
@@ -1,3 +1,5 @@
+SELECT 'ID', 'CORRELATIONID', 'MESSAGESIZE', 'MESSAGEVOLUME', 'REQUESTTYPE', 'SENDTIME', 'ISMULTITHREADED', 'GARBAGECOLLECTOR', 'RECEIVETIME'
+UNION ALL
 SELECT
     pro.id, pro.correlation_id, pro.message_size, pro.message_volume, pro.request_type, pro.send_time, IF(pro.is_multi_threaded = true, TRUE, FALSE), pro.garbage_collector, con.receive_time
 FROM
@@ -5,7 +7,7 @@ FROM
         LEFT JOIN
     consumer.message con
     ON con.correlation_id = pro.correlation_id
-INTO OUTFILE '/var/lib/mysql-files/data_profiling_test_results.csv'
+INTO OUTFILE '/var/lib/mysql-files/data_profiling_test_results_2.csv'
     FIELDS ENCLOSED BY '"'
     TERMINATED BY ';'
     ESCAPED BY '"'
