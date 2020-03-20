d04da5c6677facbf22382f9bc96e85d8295c7676
annadowling <anna.dowling@errigal.com>
2020-03-20 10:48:26 +0000

    Added jzmq_results

---
+++ .ipynb_checkpoints/exportTestResultsJZMQ.sql
@@ -0,0 +1,10 @@
+SELECT 'ID', 'CORRELATIONID', 'MESSAGESIZE', 'MESSAGEVOLUME', 'REQUESTTYPE', 'SENDTIME', 'ISMULTITHREADED', 'GARBAGECOLLECTOR', 'RECEIVETIME'
+UNION ALL
+SELECT
+    pro.id, pro.correlation_id, pro.message_size, pro.message_volume, pro.request_type, pro.send_time, IF(pro.is_multi_threaded = true, TRUE, FALSE), pro.garbage_collector, con.receive_time
+FROM
+    jzmq_producer.message pro
+        LEFT JOIN
+    jzmq_consumer.message con
+    ON con.correlation_id = pro.correlation_id
+INTO OUTFILE '/var/lib/mysql-files/profiling_test_results_jzmq.csv';
